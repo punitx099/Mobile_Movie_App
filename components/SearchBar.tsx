1673d3746cd6cc71fb46d503b23d6785cd45cca8
prashant kumar <punitchaudhary8471@gmail.com>
2025-07-22 13:37:18 +0530

    feat: for new features style: for formatting , and  logic change

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,28 +1,36 @@
-import { icons } from '@/constants/icons';
-import React from 'react';
-import { Image, TextInput, View } from 'react-native';
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
 
-interface Props{
+interface Props {
   placeholder: string;
   onPress?: () => void;
+  value: string;
+  onChangeText: (text: string) => void;
 }
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
   return (
-    <View className="flex-row items-center bg-dark-200
-    rounded-full px-5 py-4">
-     <Image source={icons.search} className="size-5"
-     resizeMode="contain" tintColor="#ab8bff" />
-     
-     <TextInput
-       onPress={onPress}
-       placeholder={placeholder}
-       value=""
-       onChangeText={() => {}}
-       placeholderTextColor="#a8b5db"
-       className="flex-1 ml-2 text-white"
-       />
+    <View
+      className="flex-row items-center bg-dark-200
+    rounded-full px-5 py-4"
+    >
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
     </View>
   );
 };
 
-export default SearchBar
+export default SearchBar;
